614058a990ff14243207a143dff093399af47317
Rebecca Quinlan <rebeccaquinlan@pc0541.elateral.internal>
2018-03-01 11:08:06 +0000

    Remove TEMP change

--- scripts/prod-status.js
+++ scripts/prod-status.js
@@ -33,7 +33,7 @@ function handleQueryResponse(response) {
      series: {
          0: { color: '#cccccc' },
          1: { color: '#f15152' },
-         2: { lineDashStyle: [5, 2], color: '' },
+         2: { lineDashStyle: [5, 2], color: '#f15152'},
          3: { type: 'bars', color: '#768e70' },
          4: { color: '#1b4079'},
          5: { lineDashStyle: [5, 2], color: '1b4079' }
