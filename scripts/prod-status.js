0e390f44211d48c6f9fb89280b3ae19f0ab75580
Rebecca Quinlan <rebeccaquinlan@pc0541.elateral.internal>
2018-03-13 10:01:32 +0000

    Update data range to A1:G11

--- scripts/prod-status.js
+++ scripts/prod-status.js
@@ -47,7 +47,7 @@
 // NEW Production Chart
 /////////////////////////////
 function drawChart() {
-  var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/16q-bSvIapnCXyqv3YU5fO4M2xDf1GxATscDXPjf8xGY/gviz/tq?gid=1078118238&range=M1:S11&headers=1');
+  var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/16q-bSvIapnCXyqv3YU5fO4M2xDf1GxATscDXPjf8xGY/gviz/tq?gid=1078118238&range=A1:G11&headers=1');
   query.send(handleQueryResponse);
 }
 
