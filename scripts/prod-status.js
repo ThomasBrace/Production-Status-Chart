cc3a05f99043fb283fa6840e15ed007c06d30430
Rebecca Quinlan <rebeccaquinlan@pc0541.elateral.internal>
2018-03-22 15:04:36 +0000

    Cosmetic tweaks, code tidy up

--- scripts/prod-status.js
+++ scripts/prod-status.js
@@ -1,49 +1,4 @@
 /////////////////////////////
-// Original Production Chart
-/////////////////////////////
-// function drawChart() {
-//   var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/16q-bSvIapnCXyqv3YU5fO4M2xDf1GxATscDXPjf8xGY/gviz/tq?gid=1078118238&range=A1:G11&headers=1');
-//   query.send(handleQueryResponse);
-// }
-//
-// function handleQueryResponse(response) {
-//   if (response.isError()) {
-//         alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-//         return;
-//       }
-//
-//   var data = response.getDataTable();
-//   data.insertColumn( 1, {
-//     type: 'string',
-//     role: 'annotation'
-//   });
-//   data.insertColumn( 2, {
-//     type: 'string',
-//     role: 'annotationText'
-//   });
-//   data.setValue(5, 1, 'March 1');
-//   data.setValue(5, 2, 'Risk free templates completed');
-//
-//   var chart = new google.visualization.LineChart(document.getElementById('combochart'));
-//   chart.draw(data, {
-//     height: '100%',
-//     width: '100%',
-//     annotations: {
-//       style: 'line'
-//      },
-//      series: {
-//          0: { color: '#cccccc' },
-//          1: { color: '#f15152' },
-//          2: { lineDashStyle: [5, 2], color: '#f15152', visibleInLegend: false },
-//          3: { type: 'bars', color: '#768e70' },
-//          4: { color: '#1b4079'},
-//          5: { lineDashStyle: [5, 2], color: '1b4079', visibleInLegend: true }
-//      },
-//      legend: {position: 'right', textStyle: {color: 'dark-gray', fontSize: 12}}
-//     });
-// }
-
-/////////////////////////////
 // NEW Production Chart
 /////////////////////////////
 function drawChart() {
@@ -90,7 +45,7 @@ function handleQueryResponse(response) {
 }
 
 /////////////////////////////
-// NEW Production Chart
+// Hour Utlization Chart
 /////////////////////////////
 function drawHoursChart() {
   var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/16q-bSvIapnCXyqv3YU5fO4M2xDf1GxATscDXPjf8xGY/gviz/tq?gid=1078118238&range=I1:K11&headers=1');
@@ -153,45 +108,18 @@ function handleRiskQueryResponse(response) {
   riskChart.draw(riskData, {
     width: 600,
     height: 400,
+    chartArea: {
+      left: 30,
+      top: 20
+    },
     slices: {
       0: { color: '#dddddd' },
       1: { color: '#cccccc' },
       2: { color: '#f15152' }
-    }
+    },
+    legend: 'bottom'
   });
 }
-
-// /////////////////////////////
-// // Dev Status Pie chart
-// /////////////////////////////
-// function drawStatusChart() {
-//   var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/16q-bSvIapnCXyqv3YU5fO4M2xDf1GxATscDXPjf8xGY/gviz/tq?gid=1313464239&range=B246:C253');
-//   query.send(handleStatusQueryResponse);
-// }
-//
-// function handleStatusQueryResponse(response) {
-//   if (response.isError()) {
-//         alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-//         return;
-//       }
-//
-//   var statusData = response.getDataTable();
-//   var statusChart = new google.visualization.PieChart(document.getElementById('status_chart'));
-//   statusChart.draw(statusData, {
-//     width: 600,
-//     height: 400,
-//     slices: {
-//       7: { color: '#f15152' }, // On Hold
-//       6: { color: '#cbdf90' }, // In Queue
-//       5: { color: '#f5a65b' }, // Change Requests (Dev)
-//       4: { color: '#768e70' }, // Template Dev
-//       3: { color: '#b87b41' }, // Change Requests (QA)
-//       2: { color: '#7f9c96' }, // Template QA
-//       1: { color: '#4d7c8a' }, // AM Review
-//       0: { color: '#1b4079' }  // Client Review
-//     }
-//   });
-// }
 
 /////////////////////////////
 // NEW Dev Status Bar chart
@@ -223,49 +151,20 @@ function handleStatusQueryResponse(response) {
 
   var statusChart = new google.visualization.BarChart(document.getElementById('status_chart'));
   statusChart.draw(statusData, {
-    width: 600,
-    height: 400,
+    width: '100%',
+    height: '100%',
     legend: 'none',
     animation: {
       duration: 1200
     },
     hAxis: {
       minValue: 8,
-    }
+    },
+    chartArea: {
+      top: 20
+    },
   });
 }
-
-/////////////////////////////
-// Overflow Status Pie Chart
-/////////////////////////////
-// function drawStudioChart() {
-//   var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/16q-bSvIapnCXyqv3YU5fO4M2xDf1GxATscDXPjf8xGY/gviz/tq?gid=1313464239&range=E246:F253');
-//   query.send(handleStudioQueryResponse);
-// }
-//
-// function handleStudioQueryResponse(response) {
-//   if (response.isError()) {
-//         alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-//         return;
-//       }
-//
-//   var studioData = response.getDataTable();
-//   var studioChart = new google.visualization.PieChart(document.getElementById('studio_chart'));
-//   studioChart.draw(studioData, {
-//     width: 600,
-//     height: 400,
-//     slices: {
-//       7: { color: '#f15152' }, // On Hold
-//       6: { color: '#cbdf90' }, // In Queue
-//       5: { color: '#f5a65b' }, // Change Requests (Dev)
-//       4: { color: '#768e70' }, // Template Dev
-//       3: { color: '#b87b41' }, // Change Requests (QA)
-//       2: { color: '#7f9c96' }, // Template QA
-//       1: { color: '#4d7c8a' }, // AM Review
-//       0: { color: '#1b4079' }  // Client Review
-//     }
-//   });
-// }
 
 /////////////////////////////
 // NEW Overflow Status Bar Chart
@@ -298,15 +197,18 @@ function handleStudioQueryResponse(response) {
   var studioChart = new google.visualization.BarChart(document.getElementById('studio_chart'));
 
   studioChart.draw(studioData, {
-    width: 600,
-    height: 400,
+    width: '100%',
+    height: '100%',
     legend: 'none',
     animation: {
       duration: 1200,
     },
     hAxis: {
       minValue: 8,
-    }
+    },
+    chartArea: {
+      top: 35
+    },
   });
 }
 
@@ -333,11 +235,15 @@ function handleWaveQueryResponse(response) {
     animation: {
       duration: 1200
     },
+    chartArea: {
+      left: 50,
+      top: 20,
+    },
     series: {
       1: { color: '#f15152' }, // On Going
       0: { color: '#dddddd' }, // Completed
     },
-    // legend: 'none'
+    legend: 'bottom'
   });
 }
 
